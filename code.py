1bc576174a24f66509fa49aeb8383577020ec268
NandanUnv <137528244+NandanUnv@users.noreply.github.com>
2023-09-11 16:01:25 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+
+import random as ra
+
+n = int(input("no.of frnds to party:"))
+li = input("Enter your friends name:")
+g = li.split(',')
+p = input("had you dinner?(y/n):")
+if p=='y' or p=='Y':
+    q = input("Ready to pay bill from random person?(y/n)")
+    if(q=='y' or q=='Y'):
+        ls = ra.randint(0,len(g)-1)
+        print(f'Here is the person "{g[ls]}" will pay bill')
+    else:
+        print("ok,pay your bill as you like!")
+else:
+    print("please have your dinner")
